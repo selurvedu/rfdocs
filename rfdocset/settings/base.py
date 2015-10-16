c0121ef7e1a943ef4ed9c2ec0ecf265987c748d3
selurvedu <selurvedu@users.noreply.github.com>
2015-10-16 21:37:51 +0000

    Remove default SECRET_KEY
    Each installation of this project must have its own SECRET_KEY.

--- rfdocset/settings/base.py
+++ rfdocset/settings/base.py
@@ -35,7 +35,7 @@ def get_env_variable(var, fail_to=None):
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '{V*b0~n`+7m*J4G&u~6lgKjD%QRefKb:UX;#<9lm5ZEgW-NW*7'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
